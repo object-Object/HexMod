d3b835cc86959dd462ff30ffe5384ae85f4cba79
object-Object <object-Object@users.noreply.github.com>
2023-11-26 23:05:36 +0000

    Deploying to gh-pages from @ object-Object/HexMod@7e3d02fe0199f4e310072463dcf9867609cba0ce 🚀

--- docs/v/0.9.6/1.0.dev20/en_us/index.js
+++ docs/v/0.9.6/1.0.dev20/en_us/index.js
@@ -131,7 +131,7 @@ function doCycleTexturesForever() {
   cycleTimeoutID = setTimeout(doCycleTexturesForever, 2000);
 }
 // these are filled by Jinja
-const BOOK_URL = "https://object-object.github.io/HexMod";
+const BOOK_URL = "https://hexcasting.hexxy.media";
 const VERSION = "0.9.6";
 const LANG = "en_us";
 // Creates an element in the form `<li><a href=${href}>${text}</a></li>`
